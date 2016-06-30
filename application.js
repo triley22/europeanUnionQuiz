08a35108f6943ce43b57222219ed34fa9d11769d
triley22 <triley09@gmail.com>
2016-06-30 14:03:52 -0700

    first commit

---
+++ application.js
@@ -0,0 +1,63 @@
+document.ready(function() {
+	var questions = [{
+		question: "In which treaty was the name 'The European Union' first used?",
+		choices: ["Paris", "Maastricht", "Amsterdam", "Germany"]
+		correct : 2,
+		fact: "The term 'Union' was introduced in the text which came out of the Maastricht Conference."
+		},
+		{
+		question: "How many members did the Union count when the Euro began circulation in January 2000?",
+		choices: ["15", "18", "9", "20"]
+		correct : 1,
+		fact: "The term 'Union' was introduced in the text which came out of the Maastricht Conference."
+		},
+		{
+		question: "How many countries were members of the Eurozone when Euro coins and banknotes were first circulated on 1st January 2002?",
+		choices: ["9", "18", "12", "8"]
+		correct : 3,
+		fact: "The term 'Union' was introduced in the text which came out of the Maastricht Conference."
+		},
+		{
+		question: "What was the former name of the currency for Europe, now known as the Euro?",
+		choices: ["E-Mark", "ECU", "EU", "Sterling"]
+		correct : 2,
+		fact: "The term 'Union' was introduced in the text which came out of the Maastricht Conference."
+		},
+		{
+		question: "The Presidency of the European Union is rotated every _ months?",
+		choices: ["6", "12", "24", "4"]
+		correct : 2,
+		fact: "The term 'Union' was introduced in the text which came out of the Maastricht Conference."	
+		},
+		{
+		question: "Who is regarded as the 'chief architect' of European Unity?",
+		choices: ["Robert Schuman", "Jean Monnet", "Winston Churchill", "Sean Connery"]
+		correct : 2,
+		fact: "Jean Omer Marie Gabriel Monnet (9 November 1888 – 16 March 1979) is regarded by many as a chief architect of European Unity and is regarded as one of its founding fathers. Never elected to public office, Monnet worked behind the scenes of American and European governments as a well-connected pragmatic internationalist."	
+		},
+		{
+		question: "Which country voted to leave the European Union shocking investors on June 23, 2016, leading to uncertainty in the financial market?",
+		choices: ["Northern Ireland", "Scotland", "Great Britain", "United Kingdom"]
+		qNum : 7,
+		correct : 3,
+		fact: "The term 'Union' was introduced in the text which came out of the Maastricht Conference."	
+		},
+	]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
